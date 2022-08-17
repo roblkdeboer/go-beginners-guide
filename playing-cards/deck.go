e1e2a5d67e12d9289ab6098f4fd6113dfa4c1e83
Rob de Boer <roblkdeboer@gmail.com>
2022-08-17 22:25:27 +0800

    Add create new deck function

--- playing-cards/deck.go
+++ playing-cards/deck.go
@@ -6,6 +6,23 @@ import "fmt"
 // which is a type of strings
 type deck []string
 
+// Must return a value of type deck
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _,suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit )
+		}
+	}
+
+	return cards
+}
+
+// d deck = receiver to receive a type deck
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
--- playing-cards/main.go
+++ playing-cards/main.go
@@ -4,13 +4,7 @@ func main() {
 	// Let Go compiler determine type for the variable
 	// Only used when defining a new variable
 	// Creating a slice - expanding list with specific types
-	cards := deck{"Ace of Diamonds", newCard()}
-	cards = append(cards, "Six of Spades")
+	cards := newDeck()
 
 	cards.print()
-}
-
-// Expect the function to return string
-func newCard() string {
-	return "Five of Diamonds"
 }
