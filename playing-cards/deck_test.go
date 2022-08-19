e1e2adbd10071bd20462ce771dee87dfb6555e90
Rob de Boer <roblkdeboer@gmail.com>
2022-08-19 17:26:27 +0800

    Write unit test for testing writing and reading from file

--- playing-cards/deck_test.go
+++ playing-cards/deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -16,4 +19,19 @@ func TestNewDeck(t *testing.T) {
 	if d[len(d) - 1] != "Four of Clubs" {
 		t.Errorf("Expected the last card to be Four of Clubs but got %s", d[len(d) - 1])
 	}
+}
+
+func TestSaveToDeckAndNewDeckTestFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != len(deck) {
+		t.Errorf("Expected %d cards in deck but got %d", len(deck), len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
 }
--- playing-cards/main.go
+++ playing-cards/main.go
@@ -5,13 +5,12 @@ func main() {
 	// Only used when defining a new variable
 	// Creating a slice - expanding list with specific types
 	cards := newDeck()
-
-	// Gets assigned according to the order the decks are returned
-	// hand, remainingDeck := deal(cards, 5)
-	// hand.print()
-	// remainingDeck.print()
-
 	// cards := newDeckFromFile("my_cards")
 	cards.shuffle()
-	cards.print()
+
+	// Gets assigned according to the order the decks are returned
+	hand, remainingDeck := deal(cards, 5)
+	hand.print()
+	remainingDeck.print()
+
 }
