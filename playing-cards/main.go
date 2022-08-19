7db98824553a75353f56f1e9f20161f20d3ea11a
Rob de Boer <roblkdeboer@gmail.com>
2022-08-19 13:42:14 +0800

    Add random number generation for shuffling

--- playing-cards/main.go
+++ playing-cards/main.go
@@ -4,13 +4,14 @@ func main() {
 	// Let Go compiler determine type for the variable
 	// Only used when defining a new variable
 	// Creating a slice - expanding list with specific types
-	// cards := newDeck()
+	cards := newDeck()
 
 	// Gets assigned according to the order the decks are returned
 	// hand, remainingDeck := deal(cards, 5)
 	// hand.print()
 	// remainingDeck.print()
 
-	cards := newDeckFromFile("my_cards")
+	// cards := newDeckFromFile("my_cards")
+	cards.shuffle()
 	cards.print()
 }
