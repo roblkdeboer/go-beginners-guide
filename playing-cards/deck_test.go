3e50bdf2d08677f87b4be13535d288e8819a1e46
Rob de Boer <roblkdeboer@gmail.com>
2022-08-19 17:17:38 +0800

    Unit tests for create new deck function

---
+++ playing-cards/deck_test.go
@@ -0,0 +1,19 @@
+package main
+
+import "testing"
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+
+	if len(d) != 16 {
+		t.Errorf("Expected deck length of 16 but got %d", len(d))
+	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected the first card to be Ace of Spades but got %s", d[0])
+	}
+
+	if d[len(d) - 1] != "Four of Clubs" {
+		t.Errorf("Expected the last card to be Four of Clubs but got %s", d[len(d) - 1])
+	}
+}
