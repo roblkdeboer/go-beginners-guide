9318133c7879de77145c04b53e04db82a3fc74d3
robdeboer <roblkdeboer@gmail.com>
2022-08-27 22:44:33 +0800

    Practice with pointers

--- structs/main.go
+++ structs/main.go
@@ -31,13 +31,17 @@ func main() {
 		},
 	}
 	
-	alexPointer := &alex
+	// & indicates the memory location of the variable alex
+	// alexPointer := &alex
 
-	alexPointer.updateName("Alexander")
+	alex.updateName("Alexander")
 	alex.print()
 }
 
+// Change receiver type to *person.  A pointer that points at a person
+// Go can automatically turn the type of person into the pointer for the person
 func (pointerToPerson *person) updateName(newFirstName string) {
+	// * gives the value that exists at the memory address
 	(*pointerToPerson).firstName = newFirstName
 }
 
